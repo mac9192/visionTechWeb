606723a3cd8941923d392f518ed207a11c9293e4
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-06-19 15:44:55 -0400

    second commit

--- front/Portfolio.jsx
+++ front/Portfolio.jsx
@@ -12,37 +12,44 @@ const Portfolio = () => {
 
       <h2 class="mx-auto max-w-screen-md text-center text-white md:text-lg">These are some of the projects that we have worked on recently. Check them out!</h2>
     </div>
-    <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
-        <h2 className="mb-4  text-center text-2xl font-bold text-white  ">GenjutsuAtl Creative Filmaking Agency</h2>
-        <h2 className="mb-4  text-center  text-2xl font-bold text-white  ">Modern Hip Hop Headless CMS Blog</h2>
-        <h2 className="mb-4  text-center  text-2xl font-bold text-white  ">Gym Warrior API App</h2>
-    </div>
+   
 
     <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
-  
-      <a href="https://genjutsuatl.com/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
+  <div> 
+      <h2 className="mb-4  text-center text-2xl font-bold text-white  ">GenjutsuAtl Creative Filmaking Agency</h2>
+           <a href="https://genjutsuatl.com/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
+     
         <Image src="/assets/gen.png" width={800} height={800} loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
         <span class="relative mr-3 mb-3 inline-block rounded-lg border border-gray-500 px-2 py-1 text-xs text-gray-200 backdrop-blur md:px-3 md:text-sm">HTML/CSS/JS</span>
       </a>
+  </div>
+ 
+    <div> 
+         <h2 className="mb-4  text-center  text-2xl font-bold text-white  ">Modern Hip Hop Headless CMS Blog</h2>
+        <a href="https://modernhiphop.vercel.app/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
     
-      <a href="https://modernhiphop.vercel.app/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
       <Image src="/assets/hip.png" width={800} height={800} loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
         <span class="relative mr-3 mb-3 inline-block rounded-lg border border-gray-500 px-2 py-1 text-xs text-gray-200 backdrop-blur md:px-3 md:text-sm">Next.Js/Hygraph CMS</span>
       </a>
-   
-      <a href="https://gymwarrior.netlify.app/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
+    </div>
+      
+   <div>
+   <h2 className="mb-4  text-center  text-2xl font-bold text-white  ">Gym Warrior API</h2>
+        <a href="https://gymwarrior.netlify.app/" class="group relative flex h-48 items-end justify-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-96">
       <Image src="/assets/warrior.png" width={800} height={800} loading="lazy" alt="Photo by Minh Pham" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
         <div class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
         <span class="relative mr-3 mb-3 inline-block rounded-lg border border-gray-500 px-2 py-1 text-xs text-gray-200 backdrop-blur md:px-3 md:text-sm">React/API</span>
       </a>
+   </div>
+     
   
     </div>
   </div>
--- front/Services.jsx
+++ front/Services.jsx
@@ -9,7 +9,7 @@ const Services = () =>{
         <section className="container mx-auto">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <div className="max-w-screen-md mb-8 lg:mb-16">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed to help your business grow</h2>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Designed to help your business grow</h2>
                 <p className="  sm:text-xl dark:text-gray-400">Here at VisionTech we provide professional services that will will create a strong web presence and growth for your brand and business</p>
             </div>
             <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-12 md:space-y-0">
@@ -17,56 +17,56 @@ const Services = () =>{
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/web1.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">Web Development</h3>
+                    <h3 className="mb-2 text-xl font-bold ">Web Development</h3>
                     <p className="  dark:text-gray-400">Web design is an essential aspect of website development as it profoundly influences the user experience, accessibility, and functionality of a site.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/ui.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">UI/UX</h3>
+                    <h3 className="mb-2 text-xl font-bold ">UI/UX</h3>
                     <p className="  dark:text-gray-400">Design a seamless, enjoyable, and efficient experience for users, which directly impacts the performance of a website.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/seo.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">SEO</h3>
+                    <h3 className="mb-2 text-xl font-bold ">SEO</h3>
                     <p className="  dark:text-gray-400">Stay competitive and maintain a strong online presence in todays increasingly digital world with effective (SEO) strategies.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/crm.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white"> CMS</h3>
+                    <h3 className="mb-2 text-xl font-bold "> CMS</h3>
                     <p className="  dark:text-gray-400"> Content management systems (CMS) have revolutionized the way websites are developed and maintained, making it easier for people to create and manage their online presence.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/writing.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white"> Copywriting</h3>
+                    <h3 className="mb-2 text-xl font-bold "> Copywriting</h3>
                     <p className="  dark:text-gray-400">Copywriting plays a vital role in website development, as it encompasses the creation of persuasive and engaging content to attract, inform, and convert visitors into customers.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/logos.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">Logos</h3>
+                    <h3 className="mb-2 text-xl font-bold ">Logos</h3>
                     <p className="  dark:text-gray-400">Logos, a critical element in website development, serve as a visual representation of a brands identity and values.</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/maintenance.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">Maintenance</h3>
+                    <h3 className="mb-2 text-xl font-bold ">Maintenance</h3>
                     <p className="  dark:text-gray-400"> By having a plan for maintenance, a web development agency can keep its business running smoothly and avoid any major disruptions..</p>
                 </div>
                 <div>
                     <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <Image className="w-16  " src = "/assets/consultation.svg" alt="/" width="400" height="350" />
                     </div>
-                    <h3 className="mb-2 text-xl font-bold dark:text-white">Consultation</h3>
+                    <h3 className="mb-2 text-xl font-bold ">Consultation</h3>
                     <p className="  dark:text-gray-400">Keep your company up to date with the latest tech trends and seek a second opinion before making any major changes.</p>
                 </div>
                
